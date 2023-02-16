8dab216c20a54336f95bff598c71bd87b6f30666
Matt Harasymczuk <matt@astrotech.io>
2023-02-16 19:53:42 +0100

    2023-02-16

--- 2023-02-16_Python-IDE/main.py
+++ 2023-02-16_Python-IDE/main.py
@@ -1,1 +1,22 @@
-URL = 'https://python.astrotech.io/_static/iris.csv'
+from time import time
+from fastapi import FastAPI
+
+
+def factorial(n: int) -> int:
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+
+def run():
+    start = time()
+    factorial(500)
+    factorial(700)
+    factorial(500)
+    factorial(800)
+    factorial(900)
+    factorial(950)
+    end = time()
+    duration = end - start
+
+    print(f'Duration: {duration} seconds')
