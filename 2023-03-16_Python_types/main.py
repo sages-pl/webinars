1636395ff0932250f2ef7a2e43ca194eba688b33
Matt Harasymczuk <matt@astrotech.io>
2023-03-16 19:53:25 +0100

    2023-02-03

--- 2023-03-16_Python_types/main.py
+++ 2023-03-16_Python_types/main.py
@@ -1,4 +1,6 @@
 # int
+# -5 do 256
+
 # float
 
 # IEEE-754
@@ -8,21 +10,10 @@
 # and - muszą oba być True
 # or - jedno z nich musi być True
 
-
 # None
+
+imie = 'Mark'
+nazwisko = 'Watney'
+wiek = None
+
 # str
-
-
-
-def add(a, b):
-    if type(a) not in (int, float):
-        raise TypeError('A może być tylko int lub float')
-
-    if type(b) not in (int, float):
-        raise TypeError('B może być tylko int lub float')
-
-    return a + b
-
-add(1, 2)        # 3
-add(1.0, 2.0)    # 3.0
-add(True, True)  # 2
