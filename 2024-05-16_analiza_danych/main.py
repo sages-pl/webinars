95308f91fcf3b719dd45ae03f7e564dcbf2226f6
Matt Harasymczuk <matt@harasymczuk.pl>
2024-05-16 18:11:23 +0200

    2024-05-16 Analiza Danych

---
+++ 2024-05-16_analiza_danych/main.py
@@ -0,0 +1,42 @@
+"""
+Python w analizie danych
+
+Język Python i biblioteki Pandas oraz Matplotlib stały się głównymi
+narzędziami wykorzystywanymi w data science. Nauczenie się korzystania
+z podstawowych funkcjonalności tych modułów nie jest trudne i każdy
+w ciągu kilku dni może zacząć z nimi pracować. Jednakże opanowanie
+trudniejszych aspektów tj. indeksy multi-kolumnowe, przecięcia poprzeczne,
+grupowania, zaawansowane agregacje, różnice przyrostowe, tabele przestawne,
+scalania i łączenia, przesunięcia-wędrujące itp., stanowią wyzwanie
+do samodzielnej nauki. I chociaż wszystkie materiały są dostępne w internecie,
+to bardzo dużo czasu można zaoszczędzić, gdy opowie o nich ktoś, kto starannie
+wyselekcjonował program, dobrał przykłady i ćwiczenia tak, aby stopniowo
+wprowadzały w zaawansowane tematy i nie przytłaczały natłokiem informacji.
+
+Zapraszam na nasz najnowszy webinar dotyczący Python, Pandas i Matplotlib.
+Podczas wykładu słuchacz zobaczy jakie umiejętności nabędzie podczas kursu
+"Analiza i wizualizacja danych w Python". Wspólnie przejdziemy studium
+przypadku analizy danych i wizualizacji. Gwarantuję, że to będzie bardzo
+owocnie poświęcone 90 minut.
+
+1. Przejdziemy studium przypadku analizy danych i wizualizacji
+2. Oczyścimy dane i przygotujemy je do wizualizacji
+3. Poznamy jak wygenerować wykresy z danych z Pandas
+
+Trener:
+https://www.sages.pl/o-nas/zespol/matt-harasymczuk
+
+Materiały:
+https://python3.info/
+
+Kurs:
+https://platforma.sages.pl/product/python-w-analizie-danych/
+https://platforma.sages.pl/python-w-analizie-danych/
+
+Kod źródłowy z dzisiejszego webinaru:
+https://github.com/sages-pl/webinars/tree/main/2024-05-16_analiza_danych
+
+Dane:
+https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_liczby_ludno%C5%9Bci
+https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_PKB_(parytet_si%C5%82y_nabywczej)
+"""
