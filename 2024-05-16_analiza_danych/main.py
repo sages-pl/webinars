5e46e989c10a1452b792c063a73435ed7d99c5c3
Matt Harasymczuk <matt@harasymczuk.pl>
2024-05-16 19:19:21 +0200

    2024-05-16 Analiza Danych

--- 2024-05-16_analiza_danych/main.py
+++ 2024-05-16_analiza_danych/main.py
@@ -40,3 +40,95 @@ Dane:
 https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_liczby_ludno%C5%9Bci
 https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_PKB_(parytet_si%C5%82y_nabywczej)
 """
+
+#%%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+LUDNOSC = 'https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_liczby_ludno%C5%9Bci'
+PKB = 'https://pl.wikipedia.org/wiki/Lista_pa%C5%84stw_%C5%9Bwiata_wed%C5%82ug_PKB_(parytet_si%C5%82y_nabywczej)'
+USD = 1
+
+#%%
+
+ludnosc = (
+    pd
+    .read_html(LUDNOSC)[0]
+    .droplevel(level=0, axis='columns')
+    .rename(columns={
+        'Państwo, obszar lub terytorium zależne': 'kraj',
+        '2022': 'ludnosc'})
+    .loc[:, ['kraj', 'ludnosc']]
+    .replace({
+        'kraj': {r'\[a\]': ''},
+        'ludnosc': {r'\[3\]': '', '\xa0': '', ' ': '', '–': pd.NA},
+    }, regex=True)
+    .replace({'kraj': {
+        'Chińska Republika Ludowa': 'Chiny',
+        'Zachodni Brzeg i Strefa Gazy': 'Zachodni Brzeg',
+        'Tajwan': pd.NA,
+    }})
+    .dropna(axis='rows', how='any')
+    .astype({'ludnosc': 'Int64', 'kraj': 'string'})
+    .convert_dtypes()
+    .set_index('kraj')
+    .sort_index(ascending=True)
+)
+
+#%%
+
+pkb = (
+    pd
+    .read_html(PKB)[0]
+    .rename(columns={
+        '2019': 'pkb',
+        'Kraj': 'kraj'})
+    .loc[:, ['kraj', 'pkb']]
+    .replace({
+        'kraj': {r'\[2\]': ''},
+        'pkb': {r'\s': '', 'b.d': pd.NA}
+    }, regex=True)
+    .astype({'pkb': 'Int64', 'kraj': 'string'})
+    .convert_dtypes()
+    .dropna(axis='rows', how='any')
+    .set_index('kraj')
+    .sort_index(ascending=True)
+    .mul(1_000_000*USD)
+)
+
+#%%
+
+result = (
+    pkb
+    .merge(ludnosc, left_index=True, right_index=True)
+    .assign(percapita=lambda df: df['pkb'] / df['ludnosc'])
+    .sort_values(by='percapita', ascending=False)
+    .head(10)
+    .loc[:, ['percapita']]
+    .round()
+    .astype('Int64')
+    .convert_dtypes()
+)
+
+#%%
+
+result.plot(
+    kind='bar',
+    title='PKB per capita',
+    xlabel='Kraj',
+    ylabel='PKB per capita [USD]',
+    figsize=(15, 10),
+    grid=False,
+)
+
+plt.show()
+
+#%%
+
+# Wykrywanie, które kraje są w obu zbiorach
+# p = pkb.index
+# l = ludnosc.index
+# query = p.isin(l)
+# p[query]
