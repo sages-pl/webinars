08ad05b55ea15ae1684b64ebd0e1bad14c145bad
Matt Harasymczuk <matt@harasymczuk.pl>
2024-12-14 06:46:16 +0100

    2024-12-14 Wykorzystanie AI w programowaniu w Python

---
+++ 2024-12-14_Coding_with_AI/main.py
@@ -0,0 +1,4 @@
+# https://python3.info/
+# https://stacja.it/produkt/bezplatnie-wykorzystanie-ai-w-programowaniu-w-python/
+# https://app.slack.com/client/T0FKGQMEZ/C082QKCS64B
+# https://monsterapi.ai/playground
